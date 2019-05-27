321b655c9b1131572df9703a3200616257182ccc
Dionrats <dionrats@gmail.com>
2019-05-27 15:08:40 +0200

    removed context test

--- src/test/java/nl/quintor/studybits/documents/DocumentsApplicationTests.java
+++ src/test/java/nl/quintor/studybits/documents/DocumentsApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DocumentsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
