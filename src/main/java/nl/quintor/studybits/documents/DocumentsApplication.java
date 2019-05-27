a0fd56bac4a4dd98714fb5971ad7a340418c2c01
Dionrats <dionrats@gmail.com>
2019-05-27 16:04:50 +0200

    moved libindy location to properties

--- src/main/java/nl/quintor/studybits/documents/DocumentsApplication.java
+++ src/main/java/nl/quintor/studybits/documents/DocumentsApplication.java
@@ -1,19 +1,23 @@
 package nl.quintor.studybits.documents;
 
 import org.hyperledger.indy.sdk.LibIndy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocumentsApplication {
 
-	//TODO Move to properties file
-	private static final String LIB_PATH = "/Users/dionrats/Nextcloud/school/Leerjaar_4/Stage/studybits/indy-sdk/libindy/target/debug";
-
+	private static String LIB_PATH;
 
 	public static void main(String[] args) {
-		LibIndy.init(LIB_PATH);
 		SpringApplication.run(DocumentsApplication.class, args);
+		LibIndy.init(LIB_PATH);
+	}
+
+	@Value("${libindy.path}")
+	public void setLibPath(String libPath) {
+		LIB_PATH = libPath;
 	}
 
 }
